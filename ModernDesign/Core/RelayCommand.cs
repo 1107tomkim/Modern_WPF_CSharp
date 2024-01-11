b5609e93f29a92e21a750b1790ecbbd23602022b
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2024-01-10 22:42:12 -0600

    Create EventHandler

--- ModernDesign/Core/RelayCommand.cs
+++ ModernDesign/Core/RelayCommand.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ModernDesign.Core
 {
-    internal class RelayCommand
+    internal class RelayCommand : ICommand
     {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        
     }
 }
